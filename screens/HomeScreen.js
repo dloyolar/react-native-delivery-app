e8c64d8989d2b55b1dc904045389ff99510ddb90
dloyolar <dieegolr@gmail.com>
2022-10-02 19:05:22 -0300

    🐳 chore: add and setup react navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="font-black">Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
