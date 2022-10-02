d4746fb94168af506fd727d0b054a7ae99f6778c
dloyolar <dieegolr@gmail.com>
2022-10-02 19:42:01 -0300

    ✨ feat(HomeScreen): header section finished

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import { StatusBar } from 'expo-status-bar';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
+      <StatusBar style="dark" />
     </NavigationContainer>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useLayoutEffect } from 'react';
+import { Image, SafeAreaView, ScrollView, Text, TextInput, View } from 'react-native';
+import {
+  ChevronDownIcon,
+  UserIcon,
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassIcon,
+} from 'react-native-heroicons/outline';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-black">Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView className="bg-white pt-5">
+      {/* Header */}
+      <View className="flex-row pb-3 item-center mx-4 space-x-2">
+        <Image
+          source={{ uri: 'https://links.papareact.com/wru' }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon color="#00CCBB" size={20} />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 ">
+        <View className="flex-row space-x-2 flex-1  bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput placeholder="Restaurants and cousines" keyboardType="default" />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+    </SafeAreaView>
   );
 };
 
