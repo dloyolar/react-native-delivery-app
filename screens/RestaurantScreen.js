8444e8edd6848418bbd269882c641a86ef1baf78
dloyolar <dieegolr@gmail.com>
2022-10-04 15:48:05 -0300

    🦄 refactor(RestaurantScreen): fix styles

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -40,7 +40,7 @@ const RestaurantScreen = () => {
         <View className="px-4 pt-4">
           <Text className="text-3xl font-bold">{title}</Text>
 
-          <View className="flex-row space-x-2 my-1">
+          <View className="flex-row space-x-2 my-1 truncate">
             <View className="flex-row items-center space-x-1">
               <StarIcon color="green" opacity={0.5} size={22} />
               <Text className="text-xs text-gray-500">
@@ -57,7 +57,7 @@ const RestaurantScreen = () => {
           <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
         </View>
 
-        <TouchableOpacity className="flex-row flex-1 items-center space-x-2 p-4 border-y border-gray-300">
+        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
           <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
           <Text className="pl-2 flex-1 text-md font-bold">Have a food allergy?</Text>
           <ChevronRightIcon color="#00CCBB" />
