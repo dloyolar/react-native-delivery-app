9017840f2e18f828fab63514e8349813780f82a8
dloyolar <dieegolr@gmail.com>
2022-10-04 19:19:31 -0300

    ✨ feat: add global state for cart with redux

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { StatusBar } from 'expo-status-bar';
 import RestaurantScreen from './screens/RestaurantScreen';
+import { store } from './store';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-      </Stack.Navigator>
-      <StatusBar style="dark" />
+      <Provider store={store}>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        </Stack.Navigator>
+        <StatusBar style="dark" />
+      </Provider>
     </NavigationContainer>
   );
 }
