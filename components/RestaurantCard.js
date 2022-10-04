2bdf2b956b7f8896f84da8b88cacbad76e800644
dloyolar <dieegolr@gmail.com>
2022-10-04 10:23:15 -0300

    ✨ feat(RestaurantCard): add navigation in onPress

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StarIcon } from 'react-native-heroicons/solid';
 import { MapPinIcon } from 'react-native-heroicons/outline';
 import { urlFor } from '../sanity';
+import { useNavigation } from '@react-navigation/native';
 
 const RestaurantCard = ({
   id,
@@ -16,8 +17,16 @@ const RestaurantCard = ({
   long,
   lat,
 }) => {
+  const props = { id, imgUrl, title, rating, genre, address, short_description, dishes, long, lat };
+  const navigation = useNavigation();
+
   return (
-    <TouchableOpacity className="bg-white mr-3 shadow w-64">
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('Restaurant', { ...props });
+      }}
+      className="bg-white mr-3 shadow w-64"
+    >
       <Image source={{ uri: urlFor(imgUrl).url() }} className="h-36 w-64 rounded-sm" />
       <View className="px-3 pb-4">
         <Text className="font-bold text-lg pt-2">{title}</Text>
